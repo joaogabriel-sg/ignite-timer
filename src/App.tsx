a0186fc12a98673a2cd62e4c2a8b4887b0da5e76
João Gabriel <main.joaogabrielsg@gmail.com>
2022-07-11 23:30:41 -0300

    feat: configure theme and global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { ThemeProvider } from "styled-components";
+
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello World</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
