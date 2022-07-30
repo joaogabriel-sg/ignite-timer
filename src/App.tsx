e7b65d99a3f2c1d26617b636ddefae57b4e3b275
João Gabriel <main.joaogabrielsg@gmail.com>
2022-07-30 17:31:09 -0300

    feat: wrap app with cycles context

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 
 import { Router } from './Router'
 
@@ -10,7 +11,9 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
